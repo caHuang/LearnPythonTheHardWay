51a77b6b9a0d0c24c21eeb9be99f9bdbaccfb0db
Yolk Huang <yolk.tw@gmail.com>
2016-04-21 21:47:19 +0800

    Exercise 34: Accessing Elements of Lists

---
+++ ex34.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+index = 1
+animal = animals[index]
+print "The animal at %d is the 2nd animal and is a %s" % (index, animal)
+
+index = 2
+animal = animals[index]
+print "the 3rd animal is at %d and is a %s" % (index, animal)
+
+index = 0
+animal = animals[index]
+print "the 1st animal is at %d and is a %s" % (index, animal)
+
+index = 3
+animal = animals[index]
+print "The animal at %d is the 4th animal and is a %s" % (index, animal)
+
+index = 4
+animal = animals[index]
+print "the 5th animal is at %d and is a %s" % (index, animal)
+
+index = 2
+animal = animals[index]
+print "The animal at %d is the 3rd animal and is a %s" % (index, animal)
+
+index = 5
+animal = animals[index]
+print "the 6th animal is at %d and is a %s" % (index, animal)
+
+index = 4
+animal = animals[index]
+print "The animal at %d is the 5th animal and is a %s" % (index, animal)
